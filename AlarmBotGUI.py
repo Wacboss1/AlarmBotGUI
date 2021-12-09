d8476b29c1d1641e1b2fa70b8fefcaf18cf43fa9
William Clemmons Jr <wacboss@live.com>
2021-12-09 15:09:14 -0600

    Got the foundation of the GUI done. Moving on to communication with the robot.

--- AlarmBotGUI.py
+++ AlarmBotGUI.py
@@ -1,6 +1,7 @@
 import socket
 import math
-import tkinter
+import matplotlib.pyplot as plt
+import numpy as np
 import time
 
 IP = '192.168.1.1'
@@ -8,15 +9,20 @@ port = 288
 speed = 115200
 bits = 8
 
-window = tkinter.Tk()
-window.title("AlarmBot UI")
-window.geometry(f'1500x1500')
+testAngles = np.deg2rad(np.array([90, 30]))
+testValues = np.array([20, 25])
 
-canvas = tkinter.Canvas(window)
-canvas.configure(bg="blue")
-canvas.pack(fill="both", expand=True)
-window.mainloop()
+def createGraph(angles, distances):
+    plot = plt.subplot(111, polar=True)
+    ticks = np.arange(0, 50, 5)
+    plot.set_ylim(0, 50)
+    plot.set_yticks(ticks)
+    plot.set_thetamin(0)
+    plot.set_thetamax(180)
+    plt.scatter(angles, distances)
+    plt.show()
 
+createGraph(testAngles, testValues)
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #     s.connect((IP, port))
 #     s.send(b'h')
